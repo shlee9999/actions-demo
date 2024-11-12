b69d39de014ad76c1951129d54eadf2aa71f02b0
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-11-12 14:19:46 +0900

    [demo] assert 추가

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,8 +3,16 @@ module.exports = {
     collect: {
       staticDistDir: './build',
       url: ['http://localhost:3000'],
-      numberOfRuns: 5,
+      numberOfRuns: 2,
     },
-    upload: { target: 'temporary-public-storage' },
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 1 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
   },
 };
